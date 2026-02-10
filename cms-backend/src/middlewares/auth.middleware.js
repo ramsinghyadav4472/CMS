efeef3c9230c563bfa45f4d4bedc8fa83859d78b
Your New Name <ramsinghyadav4472@gmail.com>
2026-02-10 21:17:47 +0530

    docs: Add comprehensive project documentation and introduce authentication middleware.

--- cms-backend/src/middlewares/auth.middleware.js
+++ cms-backend/src/middlewares/auth.middleware.js
@@ -1,8 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.header("Authorization");
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    const authHeader = req.header("Authorization");
+    if (!authHeader) return res.status(401).json({ message: "Unauthorized" });
+
+
+    const token = authHeader.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "No token provided" });
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
