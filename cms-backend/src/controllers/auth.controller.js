6a4500e06807bf767d67d6b46048c341f7998250
Your New Name <ramsinghyadav4472@gmail.com>
2026-02-10 11:10:39 +0530

    Updated Logic in auth

--- cms-backend/src/controllers/auth.controller.js
+++ cms-backend/src/controllers/auth.controller.js
@@ -49,3 +49,17 @@ exports.signup = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json({ message: "User created successfully" });
 }
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.status(400).json({ message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    res.status(200).json({ token });
+}
